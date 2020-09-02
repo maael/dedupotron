0ab31d6751578bb5578b757dcebfcdcc45446d7c
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-09-02 18:03:29 +0100

    fix: 🐛 Don't highlight full stacks

--- components/ItemIcon.tsx
+++ components/ItemIcon.tsx
@@ -8,15 +8,15 @@ const descStyles = {
   "@abilitytype": "color:red;",
 };
 const rarityColors = {
-  Junk: '#AAA',
-  Basic: '#FFFFFF',
-  Fine: '#62A4DA',
-  Masterwork: '#1a9306',
-  Rare: '#fcd00b',
-  Exotic: '#ffa405',
-  Ascended: '#fb3e8d',
-  Legendary: '#4C139D'
-}
+  Junk: "#AAA",
+  Basic: "#FFFFFF",
+  Fine: "#62A4DA",
+  Masterwork: "#1a9306",
+  Rare: "#fcd00b",
+  Exotic: "#ffa405",
+  Ascended: "#fb3e8d",
+  Legendary: "#4C139D",
+};
 function cleanDescription(desc: string) {
   return (desc || "")
     .replace(/<br>/g, "\n")
@@ -49,7 +49,6 @@ export const styles = {
   },
 };
 
-
 interface Props {
   item: any;
   selected: any;
@@ -57,63 +56,78 @@ interface Props {
   dupItems: any;
 }
 
-function ItemIcon ({item: b, setSelected, selected, dupItems}: Props) {
-  return             b && b.icon ? (
+function ItemIcon({ item: b, setSelected, selected, dupItems }: Props) {
+  const isDup = b && dupItems.has(b.id) && (!b.count || b.count < 250);
+  return b && b.icon ? (
     <Tippy
       placement="bottom-start"
       animation={false}
       content={
         <div
-        style={{
-          backgroundColor: "rgba(37,48, 43, 0.8)",
-          border: '1px solid #000000',
-          color: "#FFFFFF",
-          padding: 10,
-        }}
-      >
-        <div style={{color: rarityColors[b.rarity] || '#FFFFFF', fontWeight: 'bold', marginBottom: '0.5em'}}>
-          {b.count} {b.name}
-        </div>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: cleanDescription(b.description),
+          style={{
+            backgroundColor: "rgba(37,48, 43, 0.8)",
+            border: "1px solid #000000",
+            color: "#FFFFFF",
+            padding: 10,
           }}
-        />
-        {(b.flags || []).includes('AccountBound') ? (
-          <div style={{marginTop: '0.5em'}}>Account Bound</div>
-        ) : null}
-        {b.charges ? (
-          <div style={{marginTop: '0.5em'}}>{b.charges} charges</div>
-        ) : null}
-        {b.bound_to ? (
-          <div style={{marginTop: '0.5em'}}>Bound to: {b.bound_to}</div>
-        ) : null}
-      </div>
+        >
+          <div
+            style={{
+              color: rarityColors[b.rarity] || "#FFFFFF",
+              fontWeight: "bold",
+              marginBottom: "0.5em",
+            }}
+          >
+            {b.count} {b.name}
+          </div>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: cleanDescription(b.description),
+            }}
+          />
+          {(b.flags || []).includes("AccountBound") ? (
+            <div style={{ marginTop: "0.5em" }}>Account Bound</div>
+          ) : null}
+          {b.charges ? (
+            <div style={{ marginTop: "0.5em" }}>{b.charges} charges</div>
+          ) : null}
+          {b.bound_to ? (
+            <div style={{ marginTop: "0.5em" }}>Bound to: {b.bound_to}</div>
+          ) : null}
+        </div>
       }
       disabled={!b.name}
     >
-      <div style={{position: 'relative', display: 'inline-block'}}>
+      <div style={{ position: "relative", display: "inline-block" }}>
         <img
           src={b.icon}
-          onClick={() =>
-            setSelected(selected === b.id ? undefined : b.id)
-          }
+          onClick={() => setSelected(selected === b.id ? undefined : b.id)}
           style={{
             ...styles.icon,
-            ...(dupItems.has(b.id) ? styles.dup : {}),
+            ...(isDup ? styles.dup : {}),
             ...(selected === b.id ? styles.selected : {}),
-            ...({cursor: 'pointer'}),
-            ...(selected && selected !== b.id
-              ? styles.notSelected
-              : {}),
+            ...{ cursor: "pointer" },
+            ...(selected && selected !== b.id ? styles.notSelected : {}),
           }}
         />
-        {dupItems.has(b.id) ? <ExclamatonIcon style={{position: 'absolute', right: 5, bottom: 10, width: 20, height: 20, pointerEvents: 'none'}} fill={'#B33951'} /> : null}
+        {isDup ? (
+          <ExclamatonIcon
+            style={{
+              position: "absolute",
+              right: 5,
+              bottom: 10,
+              width: 20,
+              height: 20,
+              pointerEvents: "none",
+            }}
+            fill={"#B33951"}
+          />
+        ) : null}
       </div>
     </Tippy>
   ) : (
     <div style={styles.icon} />
-  )
+  );
 }
 
 export default memo(ItemIcon);
